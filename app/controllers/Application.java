740092892f9795a1dd48f3b05fbb57da394a1a48
Baruch Sias <baruchnoxy@gmail.com>
2015-10-24 20:51:35 -0600

    Viewing and posting comments -> Terminado

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,20 +6,38 @@ import play.mvc.*;
 import java.util.*;
 
 import models.*;
+import play.data.validation.*;
 
 public class Application extends Controller {
 
     public static void index() {
         Post frontPost = Post.find("order by postedAt desc").first();
         List<Post> olderPosts = Post.find(
-            "order by postedAt desc"
+                "order by postedAt desc"
         ).from(1).fetch(10);
         render(frontPost, olderPosts);
     }
+
     @Before
     static void addDefaults() {
-    renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
-    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
-}
+        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    }
+
+    public static void show(Long id) {
+        Post post = Post.findById(id);
+        render(post);
+    }
+
+    public static void postComment(Long postId, @Required String author,
+        @Required String content) {
+        Post post = Post.findById(postId);
+        if (validation.hasErrors()) {
+            render("Application/show.html", post);
+        }
+        post.addComment(author, content);
+        flash.success("Thanks for posting %s", author);
+        show(postId);
+    }
 
 }
